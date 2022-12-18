c0a1c8f1cbdc3cb42874e0400c41d86da44479ab
christelle <tsamo.christelle@gmail.com>
2022-12-18 02:26:45 +0000

    master deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
-
 deploy java application
-Deploy python apps
+Deploy python apps 
 deploy in the dev env
-deployment5 master
+deplyment5 master
 deployment4
-deploys to staging env
+deploy to staging env
 echo good job.
 Tested and accepted
